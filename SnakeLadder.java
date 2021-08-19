512cf42835d55f226419951cd68b8118142ae462
touhidpasha <touhidpasha552@gmail.com>
2021-08-19 16:51:11 +0530

    random number generated

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,6 +1,7 @@
 public class SnakeLadder{
     public static void main(String[] args) {
         int pos=0;
-        
+        int die=(int) ((Math.random() * (6 - 1)) + 1);
+        System.out.println(die);
     }
 }

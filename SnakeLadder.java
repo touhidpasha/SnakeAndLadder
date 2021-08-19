75ffd0c8f3fa46e887b563eb2d623ff68de3a42b
touhidpasha <touhidpasha552@gmail.com>
2021-08-19 16:56:35 +0530

    ladder and snake values added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,6 +2,8 @@ public class SnakeLadder{
     public static void main(String[] args) {
         int pos=0;
         int die=(int) ((Math.random() * (6 - 1)) + 1);
+        int[] ladder=new int[]{5,33,55,23,87,90};
+        int[] snake=new int[]{4,77,80,49};
         System.out.println(die);
     }
 }

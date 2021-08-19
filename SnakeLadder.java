fe028c90c348351aeef91813074b02a808a66af6
touhidpasha <touhidpasha552@gmail.com>
2021-08-19 17:11:33 +0530

    game played

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,9 +1,38 @@
 public class SnakeLadder{
     public static void main(String[] args) {
         int pos=0;
-        int die=(int) ((Math.random() * (6 - 1)) + 1);
+        int die,count=0;
         int[] ladder=new int[]{5,33,55,23,87,90};
         int[] snake=new int[]{4,77,80,49};
-        System.out.println(die);
+        while(count<=100)
+        {
+
+        die =(int) ((Math.random() * (6 - 1)) + 1);
+        System.out.println("your die number is "+die);
+        for (int i=0;i<ladder.length;i++)
+        {
+            if(die==ladder[i])
+            {
+                die=die+ladder[i];
+                System.out.println("you got bonus ladder "+ladder[i]);
+                break;
+            }
+        }
+
+        for (int i=0;i<snake.length;i++)
+        {
+            if(die==snake[i])
+            {
+                die=die-snake[i];
+                System.out.println("snake swalloed your point of "+snake[i]);
+                break;
+            }
+        }
+        count=count+die;
+        System.out.println("your total count is "+count);
+        
+        }
+        System.out.println("you won");
+
     }
 }

da73c7325f9811b522e16a067cbcbaf251343803
touhidpasha <touhidpasha552@gmail.com>
2021-08-19 18:01:10 +0530

    played between two players

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,14 +1,17 @@
 public class SnakeLadder{
     public static void main(String[] args) {
         int pos=0;
-        int die,count=0,dicecount=0;
+        int die,p1_count=0,p1_dicecount=0,p2_count=0,p2_dicecount=0,flag=1;
         int[] ladder=new int[]{5,33,55,23,87,90};
         int[] snake=new int[]{4,77,80,49};
-        while(count<=100)
+        while(p1_count<=100 || p2_count<=100)
         {
-
+            flag++;
+            if(flag%2==0){
+                
+            System.out.println("=================player1 data===============");
         die =(int) ((Math.random() * (6 - 1)) + 1);
-        dicecount++;
+        p1_dicecount++;
         System.out.println("your die number is "+die);
         for (int i=0;i<ladder.length;i++)
         {
@@ -29,15 +32,55 @@ public class SnakeLadder{
                 break;
             }
         }
-        count=count+die;
-        System.out.println("your total count is "+count);
-        if (count==100)
+        p1_count=p1_count+die;
+        System.out.println("your total count is "+p1_count);
+        if (p1_count==100)
         {
             System.out.println("you got exact 100 points");
         }
-        
+    }
+    else
+    {
+        System.out.println("=================player2 data===============");
+    die =(int) ((Math.random() * (6 - 1)) + 1);
+    p2_dicecount++;
+    System.out.println("your die number is "+die);
+    for (int i=0;i<ladder.length;i++)
+    {
+        if(die==ladder[i])
+        {
+            die=die+ladder[i];
+            System.out.println("you got bonus ladder "+ladder[i]);
+            break;
         }
-        System.out.println("you won and number dice thrown are "+dicecount);
+    }
+
+    for (int i=0;i<snake.length;i++)
+    {
+        if(die==snake[i])
+        {
+            die=die-snake[i];
+            System.out.println("snake swalloed your point of "+snake[i]);
+            break;
+        }
+    }
+    p2_count=p2_count+die;
+    System.out.println("your total count is "+p2_count);
+    if (p2_count==100)
+    {
+        System.out.println("you got exact 100 points");
+    }
+}
+    if(p1_count>=100 || p2_count>=100)
+    break;
+
+        }
+
+        if(p1_count>=100)
+        System.out.println("palyer1 won and number dice thrown are "+p1_dicecount);
+        else
+        System.out.println("palyer2 won and number dice thrown are "+p2_dicecount);
+
 
     }
 }
